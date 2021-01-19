27954d4094ad676f81f643a90919dca82a9b8b5b
Adriel Modollo <adrielmodollo@outlook.com>
2021-01-18 22:35:01 -0300

    gerar PDF
    Acrescentado para gerar PDF a partir de nossos dados.

---
+++ createPDF.php
@@ -0,0 +1,23 @@
+<?php
+
+use Dompdf\Dompdf;
+
+require __DIR__.'/vendor/autoload.php';
+
+// instantiate and use the dompdf class
+$dompdf = new Dompdf(["enable_remote" => true]);
+$dompdf->loadHtml("<h1>Esse é meu primeiro pdf </h1>");
+
+ob_start();
+require __DIR__ . "./exibe.php";
+$dompdf->loadHtml(ob_get_clean());
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+$dompdf->stream("documento.pdf", ["Attachment" => false]);
+
